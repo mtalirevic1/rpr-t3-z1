7d50f80fa0222da7b406f896e0a74ecea8817a48
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-02 11:45:16 +0100

    Klasa MobilniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import org.w3c.dom.ranges.RangeException;
+
+import java.util.Objects;
+
+public class MobilniBroj extends TelefonskiBroj {
+
+    private int mobilnaMreza;
+    private String broj;
+
+    MobilniBroj(int mobilnaMreza, String broj){
+        if(mobilnaMreza<60 || mobilnaMreza>67) throw new IllegalArgumentException("Mobilna mreza nije validna");
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+
+    @Override
+    public String ispisi(){
+        return "0"+mobilnaMreza+"/"+broj;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MobilniBroj)) return false;
+        MobilniBroj that = (MobilniBroj) o;
+        return mobilnaMreza == that.mobilnaMreza &&
+                Objects.equals(broj, that.broj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mobilnaMreza, broj);
+    }
+}
