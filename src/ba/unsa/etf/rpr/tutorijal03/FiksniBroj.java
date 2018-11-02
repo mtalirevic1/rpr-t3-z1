4bb95c4452fe077eb6d1efa9a0170fa2d29104be
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-02 11:21:08 +0100

    enum Grad, hashCode, ispisi i equals za FiksniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,7 +1,36 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.Objects;
+
 public class FiksniBroj extends TelefonskiBroj {
 
+    public enum Grad{
+        SARAJEVO("033"),
+        TUZLA("035"),
+        ODZAK("031"),
+        ZENICA("032"),
+        LIVNO("034"),
+        MOSTAR("036"),
+        BIHAC("037"),
+        GORAZDE("038"),
+        LJUBUSKI("039"),
+        BRCKO("049"),
+        MRKONJICGRAD("050"),
+        BANJALUKA("051"),
+        PRIJEDOR("052"),
+        DOBOJ("053"),
+        SAMAC("054"),
+        BIJELJINA("055"),
+        ZVORNIK("056"),
+        PALE("057"),
+        FOCA("058"),
+        TREBINJE("059");
+
+        private final String pozivniBroj;
+        Grad(String pozivniBroj){
+            this.pozivniBroj=pozivniBroj;
+        }
+    }
     private Grad grad;
     private String broj;
     FiksniBroj(Grad grad, String broj){
@@ -9,7 +38,22 @@ public class FiksniBroj extends TelefonskiBroj {
         this.broj=broj;
     }
 
-    /*public String ispisi(){
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FiksniBroj)) return false;
+        FiksniBroj that = (FiksniBroj) o;
+        return grad == that.grad &&
+                Objects.equals(broj, that.broj);
+    }
 
-    }*/
+    @Override
+    public int hashCode() {
+        return Objects.hash(grad, broj);
+    }
+
+    @Override
+    public String ispisi(){
+        return grad+"/"+broj;
+    }
 }
