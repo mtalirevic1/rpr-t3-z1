810fb81cf3171fc7645d8745ef68b3898a07781e
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-11-02 01:02:29 +0100

    Klasa FiksniBroj, konstruktor.

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class FiksniBroj extends TelefonskiBroj {
+
+    private Grad grad;
+    private String broj;
+    FiksniBroj(Grad grad, String broj){
+        this.grad=grad;
+        this.broj=broj;
+    }
+
+    /*public String ispisi(){
+
+    }*/
+}
