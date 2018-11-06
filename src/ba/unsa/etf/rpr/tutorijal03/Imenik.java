a4ff93c9366ce01bfee095e7e4f3aa1d4d9a3a0e
Matej <mtalirevic1@etf.unsa.ba>
2018-11-06 15:00:10 +0100

    Imenik klasa razvoj

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,10 +1,41 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class Imenik {
+
+    public enum Grad{
+        SARAJEVO("033"),
+        TUZLA("035"),
+        ODZAK("031"),
+        ZENICA("032"),
+        LIVNO("034"),
+        MOSTAR("036"),
+        BIHAC("037"),
+        GORAZDE("038"),
+        LJUBUSKI("039"),
+        BRCKO("049"),
+        MRKONJICGRAD("050"),
+        BANJALUKA("051"),
+        PRIJEDOR("052"),
+        DOBOJ("053"),
+        SAMAC("054"),
+        BIJELJINA("055"),
+        ZVORNIK("056"),
+        PALE("057"),
+        FOCA("058"),
+        TREBINJE("059");
+
+        private final String pozivniBroj;
+
+        Grad(String pozivniBroj){
+            this.pozivniBroj=pozivniBroj;
+        }
+
+        public final String getPozivniBroj(){
+            return pozivniBroj;
+        }
+    }
 
     private HashMap<String, TelefonskiBroj> mapa;
 
@@ -21,13 +52,26 @@ public class Imenik {
     }
 
     String dajIme(TelefonskiBroj broj){
-        Iterator<> it=mapa.entrySet().iterator();
+        Iterator it=mapa.entrySet().iterator();
         while(it.hasNext()){
-            Map.Entry par=it.next();
+            HashMap.Entry<String,TelefonskiBroj> par= (HashMap.Entry<String, TelefonskiBroj>) it.next();
+            if(par.getValue()==broj) return par.getKey();
         }
+        return "";
     }
 
-    String naSlovo(char s){
+    String naSlovo(char s) {
+        String ispis="";
+        int brojac=1;
+        Iterator it = mapa.entrySet().iterator();
+        while (it.hasNext()) {
+            HashMap.Entry<String, TelefonskiBroj> par = (HashMap.Entry<String, TelefonskiBroj>) it.next();
+            if(par.getKey().charAt(0)==s) ispis=ispis+brojac+". "+par.getKey()+" - "+par.getValue().ispisi()+"\n";
+        }
+        return ispis;
+    }
+
+    TreeSet<String> izGrada(Grad g){
 
     }
 }
